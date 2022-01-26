5e46a289d7a75c73b455511a097d5be7ff4aab20
iamRomeo-dev <romeo.brilland@live.fr>
2022-01-26 05:11:09 +0100

    feat: :sparkles: Init Helmet

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import "./App.css";
 import { NavBar } from "./NavBar";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 function App() {
   return (
-    <I18nextProvider i18n={i18n}>
-      <NavBar />
-    </I18nextProvider>
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | Argent Bank" />
+      <I18nextProvider i18n={i18n}>
+        <NavBar />
+      </I18nextProvider>
+    </HelmetProvider>
   );
 }
 
