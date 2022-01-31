af57441051d79ec5d576685186c37101c23396b5
iamRomeo-dev <romeo.brilland@live.fr>
2022-01-31 15:07:29 +0100

    feat: :sparkles: Init react hook forms

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,8 +1,20 @@
 /** @jsxImportSource @emotion/react */
 import UserCircleIcon from "@heroicons/react/solid/UserCircleIcon";
 import "twin.macro";
+import { useForm } from "react-hook-form";
 
 export const SignIn = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
   return (
     <main tw="flex-1 bg-gray-800">
       <section class="sign-in-content">
@@ -12,23 +24,39 @@ export const SignIn = () => {
           </div>
           <h1>Sign In</h1>
         </div>
-        <form>
+        <form
+          onSubmit={handleSubmit((data) => {
+            console.log(data);
+          })}
+        >
           <div class="input-wrapper">
             <label for="username">Username</label>
-            <input type="text" id="username" />
+            <input
+              {...register("username", {
+                required: "This is required.",
+              })}
+              type="text"
+              id="username"
+            />
+            <p>{errors.username?.message}</p>
           </div>
           <div class="input-wrapper">
             <label for="password">Password</label>
-            <input type="password" id="password" />
+            <input
+              {...register("password", {
+                required: "This is required.",
+                minLength: { value: 4, message: "Min length is 4" },
+              })}
+              type="password"
+              id="password"
+            />
+            <p>{errors.password?.message}</p>
           </div>
           <div class="input-remember">
             <input type="checkbox" id="remember-me" />
             <label for="remember-me">Remember me</label>
           </div>
-          <a href="./user.html" class="sign-in-button">
-            Sign In
-          </a>
-          <button class="sign-in-button">Sign In</button>
+          <input type="submit" value="Sing In" class="sign-in-button" />{" "}
         </form>
       </section>
     </main>
